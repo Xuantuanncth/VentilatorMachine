53d0e982d008541a0dcb1e36e0041657d08cd9e9
Tuan Do Xuan <xuantuanncth1@gmail.com>
2022-12-22 18:00:54 +0700

    Update code for profile view

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,7 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('home_page/homePage', { title: 'Express' });
+  res.render('home_page/homePage', { title: 'Hear rate monitor' });
 });
 
 module.exports = router;
