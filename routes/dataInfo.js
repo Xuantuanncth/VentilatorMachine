095c0142d0ae6bac945c3f2c7909c4112b498791
Tuan Do Xuan <xuantuanncth1@gmail.com>
2023-01-01 01:31:24 +0700

    Update title

--- routes/dataInfo.js
+++ routes/dataInfo.js
@@ -3,7 +3,7 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('data_info/dataInfo', { title: 'Express' });
+  res.render('data_info/dataInfo', { title: 'Data info' });
 });
 
 module.exports = router;
--- routes/userProfile.js
+++ routes/userProfile.js
@@ -3,7 +3,7 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('user_profile/userProfile', { title: 'Express' });
+  res.render('user_profile/userProfile', { title: 'User profile' });
 });
 
 module.exports = router;
