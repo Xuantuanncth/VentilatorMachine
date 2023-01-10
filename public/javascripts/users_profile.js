ff3fa7404688e35b191e50a831b237fa27c87ade
Tuan Do Xuan <xuantuanncth1@gmail.com>
2023-01-11 00:08:23 +0700

    Re-work update user profile

---
+++ public/javascripts/users_profile.js
@@ -0,0 +1,61 @@
+let u_name = document.getElementById('users_name');
+let u_id = document.getElementById('users_id');
+let u_date = document.getElementById('users_birthday');
+let u_address = document.getElementById('users_address');
+let u_male = document.getElementById('users_sex_male');
+let u_female = document.getElementById('users_sex_female');
+let u_info = document.getElementById('users_info');
+
+let view_name = document.getElementById('v_name');
+let view_date = document.getElementById('v_birthday');
+let view_address = document.getElementById('v_address');
+let view_sex = document.getElementById('v_sex');
+let view_age = document.getElementById('v_age');
+let view_info = document.getElementById('v_info');
+let view_avatar = document.getElementById('v_avatar');
+
+function view_info_user(user_id) {
+    console.log("[view user]: ",user_id);
+    getData(true,user_id);
+    $('#view_user_info').modal('show')
+}
+
+
+function edit_info_user(user_id){
+    console.log("[edit_user] ", user_id);
+    getData(false,user_id);
+    $('#add_new_user').modal('show')
+}
+
+function getData(is_view,users_id){
+    const url = "/userProfile/getDataUser?users_id="+users_id;
+    fetch(url).then((response)=>{
+        response.json().then((data)=>{
+            if(data.error){
+                console.log("Data error", data.error);
+            } else {
+                console.log("[data]: ",data);
+                createDataView(is_view,data)
+            }
+        })
+    });
+}
+
+function createDataView(is_view_data,data){
+    if(is_view_data){
+        view_name.innerHTML = data.users_name;
+        view_date.innerHTML = data.users_birthday;
+        view_address.innerHTML = data.users_address;
+        view_info.innerHTML= data.users_info;
+        view_sex.innerHTML = data.users_sex;
+        view_age.innerHTML = 26;
+        view_avatar.src    = data.users_avatar;
+    }  else {
+        u_name.value = data.users_name;
+        u_date.value = data.users_birthday;
+        u_address.value = data.users_address;
+        u_id.value = data.users_id;
+        u_info.value= data.users_info
+    }
+
+}
--- routes/userProfile.js
+++ routes/userProfile.js
@@ -1,9 +1,90 @@
 var express = require('express');
 var router = express.Router();
+const multer = require('multer');
+const path = require('path');
+const fs = require('fs');
+let db = require('../db');
+
+const handleError = (err, res) => {
+  res
+    .status(500)
+    .contentType("text/plain")
+    .end("Oops! Something went wrong!");
+};
+
+const upload_file = multer({
+  dest:'./database/avatar/uploads/'
+})
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('user_profile/userProfile', { title: 'User profile' });
+  try {
+    data = db.get('users').value();
+    console.log("[] =======> data:",data);
+  } catch (error) {
+    
+  }
+  res.render('user_profile/userProfile', { title: 'User profile', data:data });
 });
+
+function create_user(data, avatar){
+  data.users_avatar = avatar;
+  let is_exist_id = db.get('users').find({users_id:data.users_id}).value();
+  if(is_exist_id){
+    console.log("[create_user] ==============> id exist");
+    return false;
+  } else {
+    try {
+      db.get('users').push(data).write();
+    } catch (error) {
+      console.log("[create_user] Can not update: ",error)
+    }
+    console.log("[create_user] ============> Updated");
+    return true;
+  }
+}
+
+/* Post upload data */
+router.post('/createUser',upload_file.single('users_avatar'),(req, res, next) =>{
+  console.log("[CreateUser] ==============>", req.body);
+  const temp_path = req.file.path;
+  let user_avatar_path = '../public/images/avatar/'+req.body.users_id+'.jpg';
+  let avatar = "/images/avatar/"+req.body.users_id+'.jpg';
+  const target_path = path.join(__dirname,user_avatar_path);
+  let is_new_data = create_user(req.body, avatar);
+  if(is_new_data){
+    if(path.extname(req.file.originalname).toLowerCase() === ".jpg"){
+      fs.rename(temp_path, target_path, (err) =>{
+        if(err){
+          console.log("[err]: ",err)
+          return handleError(err, res);
+        } else {
+          res.status(200).redirect('/userProfile');
+        }
+      });
+    } else {
+      fs.unlink(temp_path,err => {
+        if(err){
+          return handleError(err, res);
+        } else {
+          res.status(403).contentType("text/plain").end("Only .png files are allowed");
+        }
+      })
+    }
+  } else {
+    res.send(" Id is exist, please correct ID");
+  }
+})
+
+router.get('/getDataUser', (req, res, next) =>{
+  console.log("GetDataUser: ", req.query.users_id);
+  try {
+    let user_data = db.get('users').find({users_id:req.query.users_id}).value();
+    console.log("User: ",user_data);
+    res.status(200).send(user_data);
+  } catch (error) {
+    res.status(403).contentType("JSON/application").end("Only .png files are allowed");
+  }
+})
 
 module.exports = router;
